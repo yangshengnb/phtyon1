65e2977c127eed0d1a9475e937090ce23409aa7d
yangshengnb <2016302172@qq.com>
2017-10-03 15:54:11 +0800

    Update main.py

--- hw01/main.py
+++ hw01/main.py
@@ -1,5 +1,7 @@
 for i in range(1, 10):
-    for j in range(1, i+1):
-      formula=f'{i:1}x{j:1}={i*j:<2}'
-      print (formula,end=' ')
-    print()
+    for x in range(1,i):
+        print(end='       ')
+    for j in range(1, 10):
+        formula=f'{i:1}x{j:1}={i*j:<2}'
+        if i<=j:print (formula,end=' ')
+    print( ) 
