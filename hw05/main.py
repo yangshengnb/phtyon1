327dbc341b24035558d8f2589c731a2cf20e7eab
yangshengnb <2016302172@qq.com>
2017-11-20 20:38:15 +0800

    Update main.py

--- hw05/main.py
+++ hw05/main.py
@@ -1,1 +1,5 @@
+import re
+txt = '我的电子邮件tom@gmail.com。将什么发送到jerry123@163.com或者3123432@qq.com。若遇特殊情况打电话给182123445678。'
 
+print(re.findall(r'[a-zA-Z0-9_]+@[a-zA-Z0-9]+\.[a-zA-Z0]+', txt))
+print(re.findall(r'[0-9]{12}', txt))
